17cf0bbc676602923a08fd4929108757dc71bed3
Hemasuganya K <hemakandhasamy1138@gmail.com>
2026-02-25 16:06:59 +0530

    Comments updated

--- inventory.py
+++ inventory.py
@@ -4,20 +4,22 @@ initial=[
   {"title": "The Alchemist", "author": "Paulo Coelho", "price": 12.99, "in_stock": 'true'},
   {"title": "1984", "author": "George Orwell", "price": 9.99, "in_stock": 'true'}
 ]
-
+# Save initial inventory
 with open("inventory.json","w")as file:
     json.dump(initial,file,indent=2)
     print("Total number of books:",len(initial))
-    
+  
+# Define new book   
 new_book = {"title": "Atomic Habits", "author": "James Clear", "price": 14.99, "in_stock": True}
 
+# Load, update, and save again
 with open("inventory.json","r")as file:
     inventory=json.load(file)
 inventory.append(new_book)
-
 with open("inventory.json", "w") as file:
     json.dump(inventory, file, indent=4)
 
+#Read the updated file again
 with open("inventory.json","r")as file:
     inventory=json.load(file)
     
